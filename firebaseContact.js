f579b24d526b66ee69f0e48a9cdf573a04ab7dc2
gurshaagrawal1211 <118744998+gurshaagrawal1211@users.noreply.github.com>
2022-12-03 19:58:31 +0530

    Create firebaseContact.js

---
+++ firebaseContact.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDR9MyOnP0CJej1D0SLEc6Ug28EUr610RI",
+    authDomain: "fir-contactpage.firebaseapp.com",
+    projectId: "fir-contactpage",
+    storageBucket: "fir-contactpage.appspot.com",
+    messagingSenderId: "220582907340",
+    appId: "1:220582907340:web:c636a4565f62153a20e169"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
